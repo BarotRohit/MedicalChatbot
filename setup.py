58d828f3d797a203e8127540e3b55b15852f2f3b
Mrbarot007 <rohitbarot99999@gmail.com>
2024-05-16 15:40:17 +0530

    Project Structure Created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'MedicalChatbot',
+    version= '0.0.0',
+    author= 'Rohit Barot',
+    author_email= 'rohitbarot99999@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
